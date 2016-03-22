4aa8cb51d4fa90a81bc055476a1a28e1f907d77c
Emmett Tsai <emmett.tsai@gmail.com>
2016-03-23 00:41:52 +0800

    Create 10035.c
    Primary Arithmetic
    #17067214

---
+++ UVa/10035.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, j, carry;
+	char a[11], b[11];
+	while (1) {
+		scanf("%d %d", &i, &j);
+		if (i == 0 && j == 0)
+			break;
+		sprintf(a, "%0*d", 10, i);
+		sprintf(b, "%0*d", 10, j);
+		carry = 0;
+		for (i = 9, j = 0; i >= 0; i--) {
+			j = (a[i] + b[i] + j >= '5' + '5');
+			carry += j;
+		}
+		if (carry) {
+			printf("%d carry operation%s.\n", carry, carry == 1 ? "" : "s");
+		} else
+			printf("No carry operation.\n");
+	}
+	return 0;
+}
