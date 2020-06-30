736c3cc8473a7b9cb7ad7f6708c8f9dab593a199
Emmett Tsai <emmett.tsai@gmail.com>
2020-06-30 21:22:34 +0800

    a229.py v3

--- ZeroJudge/a229.py
+++ ZeroJudge/a229.py
@@ -1,20 +1,19 @@
 import sys
 
-n = None
-
-def dfs(i, y, ans):
-  for j in range(i, 2 * y + 1):
-    if y == n:
+def dfs(n, i, y, ans):
+  if y == n:
+    for j in range(i, y):
       sys.stdout.buffer.write(ans[:j] + b'()' + ans[j:])
-    else:
-      dfs(j + 1, y + 1, ans[:j] + b'()' + ans[j:])
+  else:
+    for j in range(i, y):
+      dfs(n, j + 1, y + 2, ans[:j] + b'()' + ans[j:])
 
 while True:
   try:
     try:
-      n = int(input()) - 1
+      n = 2 * int(input()) - 1
     except ValueError:
       continue
-    dfs(0, 0, b'\n')
+    dfs(n, 0, 1, b'\n')
   except EOFError:
     break
